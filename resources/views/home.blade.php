7dbe353ab720a910a9f3b8cedb68da039e2fa2e2
waiyanhtunza <wyh@za.com.mm>
2024-01-27 16:30:09 +0630

    nothing change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
+    <link href="https://bootswatch.com/5/sketchy/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
     @vite('resources/css/app.css')
 </head>
 
 <body>
     <div class="flex items-center justify-center w-screen h-screen font-medium">
-        <div class="flex flex-grow items-center justify-center bg-gray-900 h-full">
+        <div class="flex flex-grow items-center justify-center bg-gray-white h-full">
             <!-- Component Start -->
             <div class="w-1/2 p-8 bg-gray-800 rounded-lg shadow-lg  text-gray-200 ">
                 <div class="flex items-center justify-center mb-10">
@@ -34,32 +36,35 @@
                         @csrf
                         @method('post')
                         <input name="task"
-                            class="flex-grow w-96 h-8 ml-4 bg-transparent focus:outline-none font-medium" type="text"
+                            class="flex-grow w-96 h-8 ml-4 bg-transparent focus:outline-none " type="text"
                             placeholder="add a new task">
 
-                        <button class="bg-gray-600 w-16 rounded">Add</button>
+                        <button class="btn btn-primary">Add</button>
                     </form>
                 </div>
 
                 <div>
                     @foreach ($tasks as $task)
-                        <div class="flex w-full">
-                            <label class="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900"
-                                for="task_10">
-                                <span
-                                    class="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full">
-                                    <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd"
-                                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                            clip-rule="evenodd" />
+                        <div class="flex  w-full">
+                            <div class=" flex items-center w-1/3  ml-10">
+                                <label class="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900"
+                                    for="task_10">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="24"
+                                        viewBox="0 -960 960 960" width="24">
+                                        <path
+                                            d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q65 0 123 19t107 53l-58 59q-38-24-81-37.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160q133 0 226.5-93.5T800-480q0-18-2-36t-6-35l65-65q11 32 17 66t6 70q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm-56-216L254-466l56-56 114 114 400-401 56 56-456 457Z" />
                                     </svg>
-                                </span>
-                                <span class="ml-4 text-sm">{{ $task->task }}</span>
 
-                            </label>
-                            <div class=" flex items-center ml-10">
-                                <p class="text-sm text-gray-600">  {{ $task->created_at->diffForHumans() }}</p>
+                                    <span class="ml-4  text-lg">{{ $task->task }}</span>
+
+                                </label>
+                            </div>
+
+                            <div class=" flex items-center justify-end ml-10">
+                                <p class="text-sm text-gray-600"> {{ $task->created_at->diffForHumans() }}</p>
+                            </div>
+                            <div>
+
                             </div>
                         </div>
                     @endforeach
